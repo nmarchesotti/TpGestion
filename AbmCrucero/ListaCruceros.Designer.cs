e32702076ba8869fbe778261c42ae39e545f9566
Phiiliip <felilotero@gmail.com>
2019-06-23 17:48:45 -0300

    Pruebas
    Todo funcionando joya
    
    * Cruceros finalizado

--- AbmCrucero/ListaCruceros.Designer.cs
+++ AbmCrucero/ListaCruceros.Designer.cs
@@ -40,7 +40,7 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(50, 208);
+            this.dataGridView1.Location = new System.Drawing.Point(27, 205);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(458, 386);
             this.dataGridView1.TabIndex = 0;
@@ -48,10 +48,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(45, 100);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(233, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(220, 25);
+            this.label1.Size = new System.Drawing.Size(364, 42);
             this.label1.TabIndex = 1;
             this.label1.Text = "Listado de cruceros";
             // 
@@ -69,7 +69,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(466, 120);
+            this.label2.Location = new System.Drawing.Point(621, 205);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(110, 25);
             this.label2.TabIndex = 19;
@@ -78,15 +78,15 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(615, 120);
+            this.comboBox1.Location = new System.Drawing.Point(550, 304);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(208, 21);
+            this.comboBox1.Size = new System.Drawing.Size(256, 21);
             this.comboBox1.TabIndex = 20;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(657, 234);
+            this.button1.Location = new System.Drawing.Point(598, 389);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(166, 58);
             this.button1.TabIndex = 21;
