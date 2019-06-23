e3df642a54b19e40e8a85948c5746eaac74ef477
Phiiliip <felilotero@gmail.com>
2019-06-23 17:40:16 -0300

    Filtros de cruceros
    * Habilitados y deshabilitados

--- AbmCrucero/ListaCruceros.Designer.cs
+++ AbmCrucero/ListaCruceros.Designer.cs
@@ -31,6 +31,9 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,10 +48,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(45, 100);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(201, 25);
+            this.label1.Size = new System.Drawing.Size(220, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Listado de cruceros";
             // 
@@ -62,11 +65,43 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(466, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Filtrar por:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(615, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(208, 21);
+            this.comboBox1.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(657, 234);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 58);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Filtrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // ListaCruceros
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(865, 630);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
@@ -83,6 +118,9 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
 
     }
 }
--- AbmCrucero/ListaCruceros.cs
+++ AbmCrucero/ListaCruceros.cs
@@ -18,12 +18,13 @@ namespace FrbaCrucero.AbmCrucero
         {
             InitializeComponent();
             inicializarLista();
+            inicializarFiltros();
         }
         
         private void inicializarLista()
         {
             var select = "SELECT * FROM LOS_QUE_VAN_A_APROBAR.ListarCruceros";
-            var c = new SqlConnection(ConfigurationManager.ConnectionStrings["GD_CRUCEROS"].ConnectionString); // Your Connection String here
+            var c = new SqlConnection(ConfigurationManager.ConnectionStrings["GD_CRUCEROS"].ConnectionString); 
             var dataAdapter = new SqlDataAdapter(select, c); 
 
             var commandBuilder = new SqlCommandBuilder(dataAdapter);
@@ -39,6 +40,46 @@ namespace FrbaCrucero.AbmCrucero
             ABMCrucero form = new ABMCrucero();
             form.Show();
             this.Dispose();
+        }
+
+        private void inicializarFiltros()
+        {
+            var dataSource = new List<String>();
+            dataSource.Add("Cruceros habilitados");
+            dataSource.Add("Cruceros fuera de servicio");
+
+            this.comboBox1.DataSource = dataSource;
+            this.comboBox1.DisplayMember = "Name";
+
+            this.comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == "Cruceros habilitados")
+            {
+                var select = "SELECT * FROM LOS_QUE_VAN_A_APROBAR.ListarCrucerosHabilitados";
+                var c = new SqlConnection(ConfigurationManager.ConnectionStrings["GD_CRUCEROS"].ConnectionString);
+                var dataAdapter = new SqlDataAdapter(select, c);
+
+                var commandBuilder = new SqlCommandBuilder(dataAdapter);
+                var ds = new DataSet();
+                dataAdapter.Fill(ds);
+                dataGridView1.ReadOnly = true;
+                dataGridView1.DataSource = ds.Tables[0];
+            }
+            else if(comboBox1.SelectedItem == "Cruceros fuera de servicio")
+            {
+                var select = "SELECT * FROM LOS_QUE_VAN_A_APROBAR.ListarCrucerosInhabilitados";
+                var c = new SqlConnection(ConfigurationManager.ConnectionStrings["GD_CRUCEROS"].ConnectionString);
+                var dataAdapter = new SqlDataAdapter(select, c);
+
+                var commandBuilder = new SqlCommandBuilder(dataAdapter);
+                var ds = new DataSet();
+                dataAdapter.Fill(ds);
+                dataGridView1.ReadOnly = true;
+                dataGridView1.DataSource = ds.Tables[0];
+            }
         }
         }
     }
