0296b3aa17c8317c92bad371c6cfba27426635b0
EmaSedlar <38228357+EmaSedlar@users.noreply.github.com>
2019-06-23 21:03:01 -0300

    Pantalla inicial

--- LoginYSeguridad/menuAdmin.cs
+++ LoginYSeguridad/menuAdmin.cs
@@ -16,5 +16,54 @@ namespace FrbaCrucero.LoginYSeguridad
         {
             InitializeComponent();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            CompraReservaPasaje.CompraReserva Form = new CompraReservaPasaje.CompraReserva();
+            Form.Show();
+            this.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AbmCrucero.ABMCrucero Form = new AbmCrucero.ABMCrucero();
+            Form.Show();
+            this.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AbmPuerto.ABMPuerto Form = new AbmPuerto.ABMPuerto();
+            Form.Show();
+            this.Dispose();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AbmRecorrido.Form1 Form = new AbmRecorrido.Form1();
+            Form.Show();
+            this.Dispose();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            AbmRol.MenuRol Form = new AbmRol.MenuRol();
+            Form.Show();
+            this.Dispose();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            GeneracionViaje.Form1 Form = new GeneracionViaje.Form1();
+            Form.Show();
+            this.Dispose();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ListadoEstadistico.Form1 Form = new ListadoEstadistico.Form1();
+            Form.Show();
+            this.Dispose();
+        }
     }
 }
