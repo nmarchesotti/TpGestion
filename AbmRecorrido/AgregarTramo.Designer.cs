daa812ad22befc4be1db5b1e843e362317490077
nmarchesotti <ncmarchesotti@gmail.com>
2019-06-24 22:39:18 -0300

    Corrección abm recorrido

---
+++ AbmRecorrido/AgregarTramo.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace FrbaCrucero.AbmRecorrido
+{
+    partial class AgregarTramo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxPuertoL = new System.Windows.Forms.ComboBox();
+            this.comboBoxPuertoS = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(155, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(192, 29);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Agregar tramos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(354, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Nuevo puerto llegada:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(52, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "Nuevo puerto salida:";
+            // 
+            // comboBoxPuertoL
+            // 
+            this.comboBoxPuertoL.FormattingEnabled = true;
+            this.comboBoxPuertoL.Location = new System.Drawing.Point(344, 156);
+            this.comboBoxPuertoL.Name = "comboBoxPuertoL";
+            this.comboBoxPuertoL.Size = new System.Drawing.Size(128, 21);
+            this.comboBoxPuertoL.TabIndex = 35;
+            // 
+            // comboBoxPuertoS
+            // 
+            this.comboBoxPuertoS.FormattingEnabled = true;
+            this.comboBoxPuertoS.Location = new System.Drawing.Point(43, 156);
+            this.comboBoxPuertoS.Name = "comboBoxPuertoS";
+            this.comboBoxPuertoS.Size = new System.Drawing.Size(128, 21);
+            this.comboBoxPuertoS.TabIndex = 34;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(203, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 52);
+            this.button1.TabIndex = 38;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AgregarTramo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 403);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBoxPuertoL);
+            this.Controls.Add(this.comboBoxPuertoS);
+            this.Controls.Add(this.label5);
+            this.Name = "AgregarTramo";
+            this.Text = "AgregarTramo";
+            this.Load += new System.EventHandler(this.AgregarTramo_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxPuertoL;
+        private System.Windows.Forms.ComboBox comboBoxPuertoS;
+        private System.Windows.Forms.Button button1;
+    }
+}
