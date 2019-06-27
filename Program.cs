012b3b9408a46de08d63795853ec48c0433b200b
Phiiliip <felilotero@gmail.com>
2019-06-27 18:26:52 -0300

    Levantar servidor y fecha
    * Desde un txt localizado en BIN/ DEBUG

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 namespace FrbaCrucero
 {
@@ -15,6 +20,29 @@ namespace FrbaCrucero
         [STAThread]
         static void Main()
         {
+            string[] lines = System.IO.File.ReadAllLines(@"AppConfig.txt");
+            string conexion = lines[0];
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.ConnectionStrings.ConnectionStrings.Remove("GD_CRUCEROS");
+            config.ConnectionStrings.ConnectionStrings.Add(new ConnectionStringSettings("GD_CRUCEROS", conexion));
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("connectionStrings");
+
+            DateTime fechaServidor = Convert.ToDateTime(lines[1]);
+
+
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["GD_CRUCEROS"].ConnectionString);
+
+            using (SqlCommand cmd = new SqlCommand("LOS_QUE_VAN_A_APROBAR.ActualizarFecha", cn))
+            {
+                cn.Open();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@Fecha", SqlDbType.DateTime2, 3).Value = fechaServidor;
+                cmd.ExecuteNonQuery();
+            }
+            cn.Close();
+            cn.Dispose();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             
